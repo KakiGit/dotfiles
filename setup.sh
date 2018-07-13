509363f29a2663e6ba1cd44edd92fd961743e77f
Jiaqi <Kaki_Github@icloud.com>
2018-07-13 13:41:28 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm .zshrc
 curl -o .zshrc https://github.com/KakiGit/myZSH/blob/master/zshrc
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
